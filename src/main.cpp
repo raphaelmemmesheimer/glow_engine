e59f4d68aeefaf2be15e6dfed30f844d3a896576
Raphael M <raphael@airglow.org>
2013-01-12 18:30:13 +0100

    merge

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,3 @@
-/*
- * main.cpp
- *
- *      Author: raphael
- *
- *      Benutzte 3rd Party Bibliotheken:
- *      gl3w 		- Extension Handling		https://github.com/skaslev/gl3w
- *      glfw2/3 	- Context, Window, Input	http://glfw.org
- *      glm			- Math						http://glm.g-truc.net
- *      stbimage	- Imageloader				http://nothings.org/
- *      OpenCTM		- CTM Model Loading			http://openctm.sourceforge.net
- *		wxWidgets	- GUI						http://wxwidgets.org
- *
- *      ///////////////////////////////////////////////////////////////
- *      Unwichtig für heute:
- *      bass		- Audio						http://www.un4seen.com/
- *      rtMidi		- Midi input				http://www.music.mcgill.ca/~gary/rtmidi/index.html#intro
- *      freenect	- Kinect input				http://openkinect.org/wiki/Main_Page
- *		tinyxml		- Xml 						http://sourceforge.net/projects/tinyxml/
- *
- *		Features:
- *		OpenGL 3.2+ / 4.0+ Core Profile
- *		-kein Immediate Mode
- *		-VBO / VAO /FBO
- *		-keine eingebauten Matrix Operationen -->  glm
- *		Texture/usr/include/wx-2.8/wx/wxScintilla
- *		Texture
- *		- über stbimageb
- *		Polygon Modelle
- *		-OpenCTM
- *		-eigene Implementierungen für OBJ, PLY im ASCII Format (feature	 incomplete)
- *		GUI
- *		-Zum Ändern verschiedener Parameter
- *		-Modelle / Texturen laden
- *		-Shader Editor							inspiriert von Shadertoy (http://www.iquilezles.org/apps/shadertoy/) von Inigo Quilez
- *		-Phong Editor
- *		-FPS Limit
- *
- *		Verschiedene Implementierte Shader
- *		-Nebel
- *		-Light Scattering						based on http://http.developer.nvidia.com/GPUGems3/gpugems3_ch13.html
- *		-SSAO		  							http://www.gamerendering.com/category/lighting/ssao-lighting/
- *		-FXAA 									http://timothylottes.blogspot.com/
- *		-Phong Shading
- *		-Diverse Post Processing Shader
- *
- *
- *		Geplante Erweiterungen
- *		-Kurven Editor, welche auf beliebige Parameter anwendbar sein sollen
- *		-Node Editor für kombination verschiedener Shader
- *		-Scene Graph
- *		-Culling / Optimierungen
- *
- *
- */
-
 #define USEWX //show gui
 #define ENABLE_FSFBO
 //#define USEROCKET
@@ -67,41 +11,15 @@
 #define GLFW_INCLUDE_GL3
 
 #include "System/Engine.h"
-#include "System/Audio.h"
 #include <stdio.h>
 
-#include <math.h>
 #include <iostream>
-
-
-#include "System/Shader.h"
-#include "System/ObjModel.h"
-#include "System/Collada.h"
-#include "System/CTMModel.h"
-#include "System/PlyModel.h"
-#include "System/Text.h"
-
-//#include "System/Controller/MidiController.h"
-
-#include "Shapes/Triangle.h"
-
-
-#include "System/Texture.h"
-#include "Shapes/Cube.h"
-
-#include "Shapes/Rect.h"
-#include "Shapes/Sphere.h"
-#include "System/FrameBufferObject.h"
-#include "System/BezierSpline.h"
-#include "System/ParticleSystem.h"
-
 
 #include <string>
 #include <iostream>
 
 #include "3rdParty/gnurocket/sync/sync.h"
 #include "Demos/DemoLossless01.h"
-//#include <freetype2/freetype/freetype.h>
 
 
 
@@ -150,15 +68,6 @@ GlowEditorfMainEditor * fMainWindow;
 
 Engine* engine;
 
-
-GLuint textureId;
-
-int pixelDataSize;
-GLuint blurTextures[6];
-GLuint pboID;
-Cube* cube;
-void *pixelData;
-
 int tmpMousePos = 0;
 
 //gnu rocket sync
@@ -168,43 +77,31 @@ const struct sync_track *lightX, *lightY, *lightZ;
 const struct sync_track *colorR, *colorG, *colorB;
 #endif
 //
-
-
-#ifdef SSDO_SCENE
-CTMModel* micronapalm;
-Quad* floorPlane;
-#endif
-
-
+Demo* demo;
 DemoLossless01 demoTesting;
 
 
 
-//ObjModel** horseGallop;
-
-//std::vector<Shader*> shaders;
-
-
 
 //pass handles over to scene
-void handleKeys(/*GLFWwindow w,*/ int key, int action)
+void handleKeys( int key, int action)
 {
 	demoTesting.getSceneManager()->getActiveScene()->handleKeyboard(key, action);
 	printf("%d \n",key);
 	switch (key){
 		case 79:
-			if (action == GLFW_PRESS)
+			if (action == GLFW_PRESS)	
 				demoTesting.getSceneManager()->switchToNextScene();
 			break;
 	}
 }
 
-void handleMousePos(/*GLFWwindow w,*/int x, int y)
+void handleMousePos(int x, int y)
 {
 	demoTesting.getSceneManager()->getActiveScene()->handleMousePos(x, y);
 }
 
-void handleScroll(/*GLFWwindow w ,*/int hor,int vert){
+void handleScroll(int hor,int vert){
 	demoTesting.getSceneManager()->getActiveScene()->handleMouseScroll(hor, vert);
 }
 
@@ -226,9 +123,9 @@ void draw()
 
 void initglfw()
 {
-  	glfwSetMousePosCallback(handleMousePos);
+	  glfwSetMousePosCallback(handleMousePos);
 
-  	glfwSetKeyCallback(handleKeys);
+	  glfwSetKeyCallback(handleKeys);
 	#ifdef GLFW3
 		glfwSetScrollCallback(handleScroll);
 	#endif
@@ -279,21 +176,21 @@ int main() { //int argc, char **argv
  
 ////xx	fMainWindow->setTextures(textures);
 
-  	engine->init(WIDTH,HEIGHT);
+	  engine->init(WIDTH,HEIGHT);
 
-  	engine->setTitle("glow:engine");
-  	demoTesting.setEngine(engine);
-  	demoTesting.init();
-    initglfw();
-  	glfwSetWindowSizeCallback(window_resize_callback);
+	  engine->setTitle("glow:engine");
+	  demoTesting.setEngine(engine);
+	  demoTesting.init();
+	initglfw();
+	  glfwSetWindowSizeCallback(window_resize_callback);
 
- 	//track->play();
+	 //track->play();
 
-    engine->setFpsLimit(60);
-    engine->setFpsLimitEnabled(true);
+	engine->setFpsLimit(60);
+	engine->setFpsLimitEnabled(true);
 
-    engine->draw = draw;
-    engine->mainLoop();
+	engine->draw = draw;
+	engine->mainLoop();
 	// Release our stream capture object
 #ifdef USEOPENCV_CAM
 	cvReleaseCapture(&pCapture);
@@ -303,7 +200,7 @@ int main() { //int argc, char **argv
 	cvDestroyAllWindows();
 #endif
 	delete engine;
-    return 0;
+	return 0;
 }
 #else
 
@@ -397,18 +294,18 @@ void DerivedFrame::OnIdle(wxIdleEvent& event)
 //	fMainWindow->mLightPosZ->GetValue().ToDouble(&lightPos[2]);
 
 
-#ifdef USEROCKET
-	sync_update(rocket,(int)floor(engine->getTime()*0.2));
+//#ifdef USEROCKET
+	//sync_update(rocket,(int)floor(engine->getTime()*0.2));
 
-	lightPos[0] = sync_get_val(lightX,engine->getTime());
-//	lightPos[1] = sync_get_val(lightY,engine->getTime());
-//	lightPos[2] = sync_get_val(lightZ,engine->getTime());
-//
-	diffuseRed = sync_get_val(colorR,engine->getTime())/255.0;
-	diffuseGreen = sync_get_val(colorG,engine->getTime())/255.0;
-	diffuseBlue = sync_get_val(colorB,engine->getTime())/255.0;
-	printf("%f %f %f \n",lightPos[0],lightPos[1],lightPos[2]);
-#endif
+	//lightPos[0] = sync_get_val(lightX,engine->getTime());
+////	lightPos[1] = sync_get_val(lightY,engine->getTime());
+////	lightPos[2] = sync_get_val(lightZ,engine->getTime());
+////
+	//diffuseRed = sync_get_val(colorR,engine->getTime())/255.0;
+	//diffuseGreen = sync_get_val(colorG,engine->getTime())/255.0;
+	//diffuseBlue = sync_get_val(colorB,engine->getTime())/255.0;
+	//printf("%f %f %f \n",lightPos[0],lightPos[1],lightPos[2]);
+//#endif
 
 	event.RequestMore();
 }
@@ -433,46 +330,52 @@ bool DerivedApp::OnInit()
 	//fMainwindow->m_SceneTree
 	//
 	//fMainWindow->setTextures(textures);
-    
+	
 	fMainWindow->m_SceneTree->AddRoot(wxString::FromAscii("Hello Word"));
-  	engine->init(WIDTH,HEIGHT);
-  	engine->setTitle("glow:engine");
+	  engine->init(WIDTH,HEIGHT);
+	  engine->setTitle("glow:engine");
 
 
-  	demoTesting.setEngine(engine);
-  	demoTesting.init();
+	//demo = new DemoLossless01();
+	demo = (Demo*)&demoTesting;
+	  demoTesting.setEngine(engine);
+	demoTesting.init();
 	demoTesting.saveToFile("lossless01.glow");
 	//demoTesting.sceneManager.toWxChoice(fMainWindow->m_choiceScene);
 
+	fMainWindow->getShaderEditor()->setShaderManager(engine->resourceManager.shaderManager);
+	fMainWindow->setResourceManager(&(engine->resourceManager));
+	fMainWindow->setDemo(demo);
+	//fMainWindow->setDemo(Demo(&demoTesting)); 
 	//engine->setMainWindow(fMainWindow); TODO: change
 	//demoTesting.getSceneManager()->getActiveScene()->getSceneGraph().toWxTree(fMainWindow->m_SceneTree);
-  	glfwSetWindowSizeCallback(window_resize_callback);
+	  glfwSetWindowSizeCallback(window_resize_callback);
 
- 	//track->play();
-  	initglfw();
+	 //track->play();
+	  initglfw();
 
-    engine->setFpsLimit(60);
-    engine->setFpsLimitEnabled(true);
+	engine->setFpsLimit(60);
+	engine->setFpsLimitEnabled(true);
 
-#ifdef USEROCKET
-	rocket = sync_create_device("sync");
-	if (!rocket)
-		printf("gnu rocket error\n");
+//#ifdef USEROCKET
+	//rocket = sync_create_device("sync");
+	//if (!rocket)
+		//printf("gnu rocket error\n");
 
-	if (sync_connect(rocket, "192.168.2.106", SYNC_DEFAULT_PORT))
-		printf("failed to connect to host\n");
+	//if (sync_connect(rocket, "192.168.2.106", SYNC_DEFAULT_PORT))
+		//printf("failed to connect to host\n");
 
 
-	 //get tracks
-	lightX = sync_get_track(rocket, "light.x");
-	lightY = sync_get_track(rocket, "light.y");
-	lightZ = sync_get_track(rocket, "light.z");
+	 ////get tracks
+	//lightX = sync_get_track(rocket, "light.x");
+	//lightY = sync_get_track(rocket, "light.y");
+	//lightZ = sync_get_track(rocket, "light.z");
 
-	colorR = sync_get_track(rocket, "color.r");
-	colorG = sync_get_track(rocket, "color.g");
-	colorB = sync_get_track(rocket, "color.b");
-#endif
-  	engine->draw = draw; //render1;
-  	return true;
+	//colorR = sync_get_track(rocket, "color.r");
+	//colorG = sync_get_track(rocket, "color.g");
+	//colorB = sync_get_track(rocket, "color.b");
+//#endif
+	  engine->draw = draw; //render1;
+	  return true;
 }
 #endif
